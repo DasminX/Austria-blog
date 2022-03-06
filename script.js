3100dee16f65bace09b843f7cdfef95f3fc17dc0
DasminX <wojtek.test99@gmail.com>
2022-03-06 14:13:06 +0100

    patch 1.1.1

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 "use strict"
+// Nav
 
 const homeBtn = document.querySelector(".home")
 
 homeBtn.addEventListener("click", () => {
   window.scrollTo(0, 0)
 })
+
+// Header
+const header = document.querySelector(".header")
+
+let idx = 0
+const srcArr = [
+  "../images/Ośrodek Narciarsi - Ischgl.jpg",
+  "../images/Ośrodek narciarski - Sölden.jpg",
+  "../images/Salzburg-1.jpeg",
+]
+
+const changePhoto = () => {
+  idx++
+  if (idx > srcArr.length - 1) idx = 0
+  header.style.backgroundImage = `url('${srcArr[idx]}')`
+  console.log(idx)
+}
+
+setInterval(changePhoto, 4000)
